b9a3ceb82c1ac2ea54951ae1139641a2b4403b1e
mishra-pankaj <immishrapankaj@gmail.com>
2023-11-10 17:01:38 +0530

    variable and date type

---
+++ variable.js
@@ -0,0 +1,11 @@
+const accountId = 144564
+let accountEmail = "hitesh@google.com"
+var accoumtPassword = "1234"
+
+console.table([accountId,accountEmail,accoumtPassword])
+//single line comment
+/* 
+multi linme comment 
+not to use var i n js as it have issues in block scope and functional 
+scope either use let
+*/
