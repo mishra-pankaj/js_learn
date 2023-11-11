9b2fae9b147afbe7fad9aff3b9c88b23193c0f72
mishra-pankaj <immishrapankaj@gmail.com>
2023-11-11 23:15:15 +0530

    new file

--- dataconversion.js
+++ dataconversion.js
@@ -1,5 +1,10 @@
 let score = "33"
-console.log(typeof(score))
+//console.log(typeof(score))
 
 let valueINNUMBER = Number(score)
-console.log(typeof valueINNUMBER)
+//console.log(typeof valueINNUMBER)
+
+//operations//
+let value = 3
+let negvalue = -value
+console.log(negvalue);
