385399a0ba51b73e3bf353ef6ae18b4b51e14b91
mishra-pankaj <immishrapankaj@gmail.com>
2023-11-14 17:15:53 +0530

    string fetures and notes

---
+++ string.js
@@ -0,0 +1,14 @@
+const name = "ramesh"
+const age = 20
+
+console.log(`hello my name is ${name} and my age is ${age}`);
+const gameNAme =new String("jite4sh")
+console.log(gameNAme)
+console.log(gameNAme[0])
+console.log(gameNAme.length)
+console.log(gameNAme.toUpperCase)
+console.log(gameNAme.charAt(3))
+console.log(gameNAme.indexOf("s"))
+
+const user_id = "  1132324    "
+console.log(user_id.trim())
