26fb7a288d5d23b0cee1c73339c0ea28e99c6e7a
mishra-pankaj <immishrapankaj@gmail.com>
2023-11-22 09:28:50 +0530

    mathandnumber

---
+++ number_math.js
@@ -0,0 +1,13 @@
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toFixed(1))
+console.log(balance.toFixed(2))
+const Othernumber = 23.34534
+console.log(Othernumber.toPrecision(3))
+const hundreads = 10000000
+console.log(hundreads.toLocaleString("en-IN"));
+console.log(Math.abs(-4));
+console.log(Math.round(2.34));
+console.log(Math.ceil(2.34));
+console.log(Math.floor(2.34));
+
