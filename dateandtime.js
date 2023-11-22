4969597436c80df688d9afc1c58019e63e7868e5
mishra-pankaj <immishrapankaj@gmail.com>
2023-11-22 09:52:23 +0530

    dateandtime

---
+++ dateandtime.js
@@ -0,0 +1,10 @@
+let mydate = new Date()
+/*console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toJSON());*/
+//month 0 se start hota hai js me 
+let mycreateddate = new Date(2023,10,22)
+console.log(mycreateddate.toDateString());
+
+let mytimestamp = Date.now()
+console.log(mytimestamp);
