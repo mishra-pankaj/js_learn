f18cbe04f6545c560579e3f0d1578110f19cd10c
mishra-pankaj <immishrapankaj@gmail.com>
2023-12-28 11:45:45 +0530

    conversion of datatype

---
+++ new/data_con.js
@@ -0,0 +1,16 @@
+let score="33"
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+
+// "33"= 33
+// "33sds"= NAN
+//true = 1; false=0
+
+let isloggedIN = 1
+let bolleanIsloggedIn = Boolean(isloggedIN)
+console.log(bolleanIsloggedIn);
+
+//1 - true
+//0 - false 
+// "" - false 
+//"absd " - true
